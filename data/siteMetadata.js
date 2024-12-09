5cfb5257c1858fb88f5ff823b327286d789680b6
Kshitij Raj Sharma <36752999+kshitijrajsharma@users.noreply.github.com>
2024-12-09 21:55:52 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,11 +1,11 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  //title: 'Next.js Starter Blog',
-  author: 'Omowonuola Akintola',
-  headerTitle: 'Omowonuola A.',
+  title: 'Ola Blog',
+  author: 'Omoonuola Akintola',
+  headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
-  theme: 'light', // system, dark or light
+  theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,

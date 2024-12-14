56004e6f0c12cfbe681221556a7170ec64b999ea
Omowonuola A. <59511329+Omowonuola-Akintola@users.noreply.github.com>
2024-12-14 09:46:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-'use client'
-import Greetings from '@/components/Greetings'
+import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import Main from './Main'
 
-export default function Home() {
-  return (
-    <>
-      <Greetings />
-    </>
-  )
+export default async function Page() {
+  const sortedPosts = sortPosts(allBlogs)
+  const posts = allCoreContent(sortedPosts)
+  return <Main posts={posts} />
 }

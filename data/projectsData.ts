3792144ea012948c499b161bda5231315306d001
Omowonuola-Akintola <omowonuola52@gmail.com>
2025-08-29 12:20:00 +0200

    adding some school projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,26 @@ interface Project {
 
 const projectsData: Project[] = [
 
+
+  {
+    title: 'Sar Superpixel Classification',
+    description: `This project implements an object-based image analysis (OBIA) workflow for SAR image segmentation and classification 
+    using superpixels and Random Forest (RF) classification. The goal is to distinguish water and non-water regions in synthetic aperture 
+    radar (SAR) imagery.`,
+    imgSrc: '/static/images/Bangladesh.jpeg',
+    href: 'https://github.com/Omowonuola-Akintola/sar-superpixel-classification',
+  },
+
+  {
+    title: 'SBAS-InSAR Dam Deformation Monitoring',
+    description: `Monitoring and analysis of surface deformation from July 2022 to October 2024. Motivated by reports of a 
+    potential dam collapse in 2022 and subsequent flooding in September 2024, Sentinel-1 ascending orbit SAR data from July 
+    2022 to October 2024 were processed to generate interferograms for the time series analysis. Time-series deformation and 
+    velocity maps were generated to detect any precursory ground movements that may have signaled structural instability.`,
+    imgSrc: '/static/images/velocitymap.jpeg',
+    href: 'https://github.com/Omowonuola-Akintola/sbas-insar-dam-monitoring',
+  },
+  
   {
     title: 'The Women Learn Program',
     description: `In 2023, I piloted the Women Learn Program (WLP) through The GIS Girl, a social impact initiative that
